07cdc969d3bdd972e24b990a6ef27b86a23e11ec
kim <bear2u@naver.com>
2021-01-08 18:22:48 +0900

    fix

--- src/components/ui/Header.js
+++ src/components/ui/Header.js
@@ -10,7 +10,9 @@ import Tab from "@material-ui/core/Tab";
 
 import logo from "../../assets/logo.svg";
 
-import { Link } from 'react-router-dom';
+import { Link } from "react-router-dom";
+import Menu from "@material-ui/core/Menu";
+import MenuItem from "@material-ui/core/MenuItem";
 
 function ElevationScroll(props) {
   const { children, window } = props;
@@ -37,10 +39,10 @@ const useStyles = makeStyles((theme) => ({
     height: "7em",
   },
   logoContainer: {
-      padding: 0,
-      "&:hover": {
-          backgroundColor: "transparent"
-      }
+    padding: 0,
+    "&:hover": {
+      backgroundColor: "transparent",
+    },
   },
   tabContainer: {
     marginLeft: "auto",
@@ -64,25 +66,37 @@ export default function Header(props) {
   const classes = useStyles();
 
   const [value, setValue] = useState(0);
+  const [anchorEl, setAnchorEl] = useState(null);
+  const [open, setOpen] = useState(false);
 
   const handleChange = (e, value) => {
     setValue(value);
   };
 
+  const handleClick = (e) => {
+    setAnchorEl(e.currentTarget);
+    setOpen(true);
+  };
+
+  const handleClose = (e) => {
+    setAnchorEl(null);
+    setOpen(false);
+  };
+
   useEffect(() => {
-      if(window.location.pathname === "/" && value !== 0) {
-        setValue(0);
-      } else if(window.location.pathname === "/services" && value !== 1) {
-        setValue(1);
-      } else if(window.location.pathname === "/revolution" && value !== 2) {
-        setValue(2);
-      } else if(window.location.pathname === "/about" && value !== 3) {
-        setValue(3);
-      } else if(window.location.pathname === "/contact" && value !== 4) {
-        setValue(4);
-      } else if(window.location.pathname === "/estimate" && value !== 5) {
-        setValue(5);
-      }
+    if (window.location.pathname === "/" && value !== 0) {
+      setValue(0);
+    } else if (window.location.pathname === "/services" && value !== 1) {
+      setValue(1);
+    } else if (window.location.pathname === "/revolution" && value !== 2) {
+      setValue(2);
+    } else if (window.location.pathname === "/about" && value !== 3) {
+      setValue(3);
+    } else if (window.location.pathname === "/contact" && value !== 4) {
+      setValue(4);
+    } else if (window.location.pathname === "/estimate" && value !== 5) {
+      setValue(5);
+    }
   }, [value]);
 
   return (
@@ -90,20 +104,54 @@ export default function Header(props) {
       <ElevationScroll>
         <AppBar position="fixed">
           <Toolbar disableGutters>
-            <Button component={Link} to="/" onClick={() => setValue(0)} className={classes.logoContainer} disableRipple>
-                <img alt="Company logo" className={classes.logo} src={logo} />
-            </Button>            
+            <Button
+              component={Link}
+              to="/"
+              onClick={() => setValue(0)}
+              className={classes.logoContainer}
+              disableRipple
+            >
+              <img alt="Company logo" className={classes.logo} src={logo} />
+            </Button>
             <Tabs
               value={value}
               onChange={handleChange}
               className={classes.tabContainer}
               indicatorColor="primary"
             >
-              <Tab className={classes.tab} component={Link} to="/" label="Home" />
-              <Tab className={classes.tab} component={Link} to="/services" label="Services" />
-              <Tab className={classes.tab} component={Link} to="/revolution" label="The Revolution" />
-              <Tab className={classes.tab} component={Link} to="/about" label="About Us" />
-              <Tab className={classes.tab} component={Link} to="/contact" label="Contact Us" />
+              <Tab
+                className={classes.tab}
+                component={Link}
+                to="/"
+                label="Home"                
+              />
+              <Tab
+                className={classes.tab}
+                component={Link}
+                to="/services"
+                label="Services"
+                aria-owns={anchorEl ? "simple-menu" : undefined}
+                aria-haspopup={anchorEl ? "true" : undefined}                
+                onMouseOver={e => handleClick(e)}
+              />
+              <Tab
+                className={classes.tab}
+                component={Link}
+                to="/revolution"
+                label="The Revolution"
+              />
+              <Tab
+                className={classes.tab}
+                component={Link}
+                to="/about"
+                label="About Us"
+              />
+              <Tab
+                className={classes.tab}
+                component={Link}
+                to="/contact"
+                label="Contact Us"
+              />
             </Tabs>
             <Button
               variant="contained"
@@ -114,6 +162,19 @@ export default function Header(props) {
             >
               Free Estimate
             </Button>
+            <Menu
+              id="simple-menu"
+              anchorEl={anchorEl}
+              open={open}
+              onClose={handleClose}
+              MenuListProps={{onMouseLeave: handleClose}}
+            >
+              <MenuItem onClick={() => { handleClose(); setValue(1); }} component={Link} to="/customsoftware">
+                Custom Software Development
+              </MenuItem>
+              <MenuItem onClick={() => { handleClose(); setValue(1); }} to="/mobileapps">Mobile App Development</MenuItem>
+              <MenuItem onClick={() => { handleClose(); setValue(1); }} to="/website">Website App Development</MenuItem>
+            </Menu>
           </Toolbar>
         </AppBar>
       </ElevationScroll>
